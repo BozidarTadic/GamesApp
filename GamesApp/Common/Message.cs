a334b5a65aed267c8e199916aa7d5ba890dca26d
tadicbb <tadicbb@gmail.com>
2022-01-07 14:03:35 +0100

    add error mesages

---
+++ GamesApp/Common/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesApp.Common
+{
+    public class Message
+    {
+        public static List<ErrorResult> InternalServerError = new List<ErrorResult>
+        {
+            new ErrorResult
+            {
+                ErrorCode = "Internal server error",
+                ErrorDescription = "Internal server error."
+            }
+        };
+
+    }
+}
